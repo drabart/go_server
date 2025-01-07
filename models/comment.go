6778c9681bd01e0219e45ddff88656726ae61b62
Bartosz Drabiński <drabart@outlook.com>
2025-01-07 23:39:19 +0100

    updated the models

--- models/comment.go
+++ models/comment.go
@@ -1,1 +1,15 @@
 package models
+
+import "gorm.io/gorm"
+
+type Comment struct {
+	gorm.Model
+	Contents   string
+	UserID     uint
+	User       User
+	TutorialID uint
+	Tutorial   Tutorial
+	// comment might be a reply to another comment
+	ParentID uint
+	Parent   *Comment `gorm:"foreignkey:ParentID"`
+}
--- models/tutorial.go
+++ models/tutorial.go
@@ -1,1 +1,11 @@
 package models
+
+import "gorm.io/gorm"
+
+type Tutorial struct {
+	gorm.Model
+	Name        string
+	Description string
+	URL         string
+	Tags        []Tag `gorm:"many2many:tutorial_tags;"`
+}
